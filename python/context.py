795e59f2f387d848487853752e602125fc9d1be0
smkycat <catherinehan714@gmail.com>
2023-10-28 22:16:06 -0700

    Pip freeze

--- python/context.py
+++ python/context.py
@@ -1,2 +1,2 @@
 context = 'Today, we are finally back to streaming after a 2 week hiatus!'
-context = 'Right now, you are lurking in your discord server, Alluring Luna Abyss!'
+# context = 'Right now, you are lurking in your discord server, Alluring Luna Abyss!'
